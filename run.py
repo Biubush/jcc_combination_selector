025d6ac161ca23a0b7e9f70673722dbbd3389103
Biubush <1577141450@qq.com>
2024-10-12 13:34:25 +0800

    检查数据缓存，减少资源占用

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from ChessOptimizer.DataTransformer import get_newest_data, processAndCleanData
 from ChessOptimizer.Optimizer.advanced import genetic_algorithm_optimizer
+import os
 
 if __name__ == "__main__":
     original_src_folder = "src/original"
     processed_src_folder = "src/processed"
-    # get_newest_data(original_src_folder)
-    # processAndCleanData(original_src_folder, processed_src_folder)
+    if not os.path.exists(original_src_folder):
+        get_newest_data(original_src_folder)
+        processAndCleanData(original_src_folder, processed_src_folder)
     genetic_algorithm_optimizer(processed_src_folder)
